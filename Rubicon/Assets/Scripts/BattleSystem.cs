4bad4fc2a79efc0f85f016312d2a01af90195f69
Noah Wilson <noahwilsonmailbox32@gmail.com>
2021-06-02 14:16:11 -0400

    ADDED UI

---
+++ Rubicon/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateSystem {START,PLAYER,ENEMY,WON,LOST }
+public class BattleSystem : MonoBehaviour
+{
+    public StateSystem state;
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerPlatform;
+    public Transform enemyPlatform;
+
+    private Unit playerUnit;
+    private Unit enemyUnit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = StateSystem.START;
+        SetUpBattle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetUpBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerPlatform);
+        playerUnit = playerGO.GetComponent<Unit>();
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyPlatform);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+    }
+}
---
+++ Rubicon/Assets/Scripts/Unit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int maxHealth;
+    public int currentHealth;
+    public int damage;
+}
