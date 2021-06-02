64fadf1e774df5e53e89c2d26e22645996aa6cdf
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-06-02 13:41:10 -0400

    Started Project

---
+++ Rubicon/Assets/Scripts/InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+
+    }
+    public void Kick()
+    {
+        anim.SetTrigger("Kick");
+    }
+    public void Swing()
+    {
+        anim.SetTrigger("Swing");
+    }
+    public void Defend()
+    {
+        anim.SetTrigger("Defend");
+    }
+}
